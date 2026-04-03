6c3937fca665371569044a70245b2e535dc06e69
Devanshi-Vadiya <devanshivadiya@gmail.com>
2026-04-03 14:36:07 +0530

    first draft

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,7 +6,7 @@ import DashboardLayout from '../layouts/DashboardLayout';
 import ProtectedRoute from '../components/ProtectedRoute';
 import LoadingSpinner from '../components/LoadingSpinner';
 
-// Lazy loaded pages for performancezzzz
+// Lazy loaded pages for performancezzzzz
 const Home = lazy(() => import('../pages/Home'));
 const Blogs = lazy(() => import('../pages/Blogs'));
 const BlogDetail = lazy(() => import('../pages/BlogDetail'));
