a6b091bc4b5f24f02df1686b539aad8e04805952
Devanshi-Vadiya <devanshivadiya@gmail.com>
2026-04-07 14:04:21 +0530

    add

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -82,7 +82,6 @@ export default function Courses() {
         </p>
 
         {/* Grid */}
-        
         {filtered.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
             {filtered.map((course, i) => (
