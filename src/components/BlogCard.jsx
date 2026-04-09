89fab6e1b02ad123d83fd2e9990a8d0170ff3466
Devanshi-Vadiya <devanshivadiya@gmail.com>
2026-04-09 16:56:14 +0530

    add

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -37,7 +37,7 @@ export default function BlogCard({ blog, index = 0 }) {
         </div>
       </div>
 
-      {/* Contentdxxxxjsnj*/} 
+      {/* Content*/} 
       <div className="p-5">
         <div className="flex items-center gap-2 mb-3">
           <img
